db56a830518448f4f3315dd3759159e8102e7bd8
RomanKuzhekov <profet@inbox.ru>
2022-03-06 21:43:05 +0300

    Создал конфиг приложения

---
+++ config/config.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Роман
+ * Date: 06.03.2022
+ * Time: 21:34
+ */
+
+return [
+    'root_dir' => $_SERVER['DOCUMENT_ROOT']."/../",
+    'controller_namespace' => 'app\controllers\\',
+    'defaultController' => 'product',
+    'defaultAction' => 'index',
+    'useLayout' => true,
+    'layout' => 'main',
+    'components' => [
+        'db' => [],
+        'main' => [],
+        'request' => [],
+        'comment' => [],
+        'product' => [],
+        'user' => [],
+        'category' => [],
+        'shop' => [],
+    ]
+];
+
+
+
+
