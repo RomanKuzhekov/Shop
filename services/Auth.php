c6ea58c74702e46c47df17e7814a1c6d20ababaf
RomanKuzhekov <profet@inbox.ru>
2022-03-10 10:35:21 +0300

    Класс для авторизации

---
+++ services/Auth.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Роман
+ * Date: 10.03.2022
+ * Time: 10:31
+ */
+
+namespace app\services;
+
+
+class Auth
+{
+    protected $sessionKey = 'sid';
+
+    public function getSessionId()
+    {
+
+    }
+
+    public function openSession()
+    {
+
+    }
+
+    public function generateStr()
+    {
+
+    }
+}
