f983482e7d9bfbe73d0f280257194aa08f269fca
RomanKuzhekov <profet@inbox.ru>
2022-03-10 15:36:59 +0300

    Работа с корзиной

--- controllers/BasketController.php
+++ controllers/BasketController.php
@@ -9,7 +9,121 @@
 namespace app\controllers;
 
 
+/**
+ * Полная корзина, Заказ товаров
+ * Вывод корзины на экран, пересчет количества, удаление товаров, очистка корзины
+ * Подтверждение заказа
+ *
+ * Class BasketController
+ * @package app\controllers
+ */
 class BasketController
 {
+    private $fullBasket;
+    private $data;
+    private $products;
 
+    /**
+     * BasketController constructor.
+     * @param null $renderer
+     */
+    public function __construct(IRenderer $renderer = null)
+    {
+        $this->renderer = $renderer;
+        $this->fullBasket = $this->getModel()->getBasket($this->getIdBasket());
+        $this->prepareBasket();
+    }
+
+    public function actionFull()
+    {
+        $priceAll = $amountAll = 0;
+        foreach ($this->fullBasket as $item) {
+            $this->products[$item->id_product] = [
+                'id' => $item->id_product,
+                'name' => $item->name_product,
+                'price' => $item->price,
+                'count' => $item->amount
+            ];
+
+            $amountAll += $item->amount;
+            $priceAll += $item->price * $item->amount;
+        }
+
+        $this->data = [
+            'countAll' => $amountAll,
+            'price' => $priceAll
+        ];
+
+        echo $this->render("{$this->controllerName}/$this->actionName", [
+            'products' => $this->products,
+            'data' => $this->data
+        ]);
+
+    }
+
+    public function actionOrder()
+    {
+        if ($_SERVER['REQUEST_METHOD'] == "POST") {
+            if ($this->getModel()->updateOrderByBasket($this->getIdBasket(), $_POST['export'], $_POST['payment'])) {
+                $date = new \DateTime('-30 days');
+                $this->getModel()->clearDays($date->format('Y-m-d'));
+                $message = true;
+            }
+        }
+        echo $this->render("{$this->controllerName}/$this->actionName", [
+            'message' => $message
+        ]);
+    }
+
+    public function actionOrders()
+    {
+        $products = $this->getModel()->getBasket($this->getIdBasket(), 1);
+        if (empty($products)) {
+            $products = false;
+        }
+        echo $this->render("{$this->controllerName}/$this->actionName", [
+            'products' => $products
+        ]);
+    }
+
+    private function prepareBasket()
+    {
+        if ($_SERVER['REQUEST_METHOD'] == "POST") {
+            if ($_POST['clear']) {
+                $this->getModel()->clear('id_basket', $this->getIdBasket());
+                $this->redirect('./');
+            } elseif ($_POST['submit']) {
+                if ($_POST['delete']) {
+
+                    foreach ($_POST['delete'] as $item) {
+                        $this->getModel()->deleteProductByBasket($this->getIdBasket(), $item);
+                    }
+
+                    if (empty($this->getModel()->getBasket($this->getIdBasket()))) {
+                        $this->redirect('./');
+                    } else {
+                        $this->redirect("./basket/full/");
+                    }
+                }
+
+                if ($_POST['count']) {
+                    foreach ($_POST['count'] as $key => $value) {
+                        $this->getModel()->updateProductByBasket($this->getIdBasket(), $key, $value);
+                    }
+
+                    $this->fullBasket = $this->getModel()->getBasket($this->getIdBasket());
+                }
+            }
+        }
+    }
+
+    private function getModel()
+    {
+        return new Basket();
+    }
+
+    private function getIdBasket()
+    {
+        return App::call()->shop->idBasket;
+    }
 }
--- models/Basket.php
+++ models/Basket.php
@@ -8,8 +8,72 @@
 
 namespace app\models;
 
-
+/**
+ * Корзина
+ *
+ * Class Basket
+ * @package app\models
+ * @property int id_basket
+ * @property int id_product
+ * @property string name_product
+ * @property int price
+ */
 class Basket
 {
+    protected static $fields = [
+        'id_basket',
+        'id_product',
+        'name_product',
+        'price',
+        'created_at'
+    ];
 
+    public function __construct()
+    {
+        parent::__construct();
+        $this->tableName = 'basket';
+        $this->entityClass = Basket::class;
+    }
+
+    public function getBasket($idBasket, $flag = 0)
+    {
+        return $this->conn->fetchAll("SELECT * FROM {$this->tableName} WHERE id_basket = $idBasket AND status = $flag ORDER BY id_product",
+            $this->entityClass
+        );
+    }
+
+    public function getProductByBasket($idBasket, $idProduct)
+    {
+        return $this->conn->fetchAll("SELECT * FROM {$this->tableName} WHERE id_basket = $idBasket AND id_product = $idProduct AND status = 0",
+            $this->entityClass
+        );
+    }
+
+    public function clear($nameColumn, $valueColumn)
+    {
+        return $this->conn->execute("DELETE FROM {$this->tableName} WHERE $nameColumn = $valueColumn  AND status = 0");
+    }
+
+    public function clearDays($date)
+    {
+        return $this->conn->execute("DELETE FROM {$this->tableName} WHERE created_at < '$date'  AND status = 0");
+    }
+
+    public function deleteProductByBasket($idBasket, $idProduct)
+    {
+        return $this->conn->execute("DELETE FROM {$this->tableName} WHERE id_basket = $idBasket AND id_product = $idProduct AND status = 0");
+    }
+
+    public function updateProductByBasket($idBasket, $idProduct, $amountBasket, $flag = 0)
+    {
+        if ($flag == 1) {
+            $amountBasket = "amount + $amountBasket";
+        }
+        return $this->conn->execute("UPDATE {$this->tableName} SET amount = $amountBasket WHERE id_basket = $idBasket AND id_product = $idProduct AND status = 0");
+    }
+
+    public function updateOrderByBasket($idBasket, $delivery, $payment)
+    {
+        return $this->conn->execute("UPDATE {$this->tableName} SET status = 1, delivery = '$delivery', payment = '$payment' WHERE id_basket = $idBasket AND status = 0");
+    }
 }
