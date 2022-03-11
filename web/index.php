0b065f4778b908aecbb08a8302a9061b2f0e2679
RomanKuzhekov <profet@inbox.ru>
2022-03-11 13:31:44 +0300

    Завершил разработку

--- web/index.php
+++ web/index.php
@@ -1,20 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Роман
- * Date: 06.03.2022
- * Time: 20:14
- */
-
-//ini_set('display_errors', 'on');
-//error_reporting(E_ALL);
 
 require_once "../base/App.php";
 \app\base\App::call()->run();
-
-
-
-//$a = new \app\models\Product();
-////var_dump($a);
-////var_dump($a->getConnection());
-//var_dump($a->getOne(1));
